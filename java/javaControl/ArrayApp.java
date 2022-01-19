51fb2ccda89eaef41c28cfb2b7036c5a29f44475
LAPTOP-0B5VH4GD\1 <LAPTOP-0B5VH4GD\1>
2022-01-19 19:21:19 +0900

    java Class Instance

---
+++ java/javaControl/ArrayApp.java
@@ -0,0 +1,10 @@
+
+public class ArrayApp {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
---
+++ java/javaControl/ComparisonOperatorApp.java
@@ -0,0 +1,15 @@
+
+public class ComparisonOperatorApp {
+
+	public static void main(String[] args) {
+
+		System.out.println(1>1);
+		System.out.println(1==1);
+		System.out.println(1<1);
+		System.out.println(1>=1);
+
+
+		
+	}
+
+}
